ff5a59d75680669298faf49ce23337d24cc0d17f
Oleksiy Lazarenko <alexusunit@gmail.com>
2023-12-10 11:57:42 +0000

    deploy

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,7 +25,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+if config('PROD') == 'True':
+    DEBUG = False
+else:
+    DEBUG = True
 
 ALLOWED_HOSTS = ['boutique-ado-heroku-b8518a253be7.herokuapp.com', '127.0.0.1']
 
